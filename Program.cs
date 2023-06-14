092dbf995b83e8af6c2dc15184246e018e006076
olgakop9 <112979362+olgakop9@users.noreply.github.com>
2023-06-14 20:51:42 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿//Написать программу, которая из сформированного массива строк содержащих символы и числа создать массив из строк, 
+//в который попадут только символы, не являющиеця цифрами.
+//Входные данные можно получить путём ввода с клавиатуры, но лучше сделать автогенерацию данных. При решении не использовать "читерство".
+
+using System;
+using System.Linq;
+class Program
+{
+    public Program()
+    {
+    }
+
+    static void Main()
+    {
+        string[] originalArray = { "Hello123", "World456", "Today is 2023.05.21" };
+        string[] newArray = originalArray.Select(s => new string(s.Where(c => !char.IsDigit(c)).ToArray())).ToArray();
+        Console.WriteLine(string.Join(", ", newArray));
+    }
+}
